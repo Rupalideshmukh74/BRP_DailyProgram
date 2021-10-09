9078ed8726c578a2ca2ca7d7df17a08e999849e5
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-10-09 06:29:09 +0530

    [Rupali]feat:Added Buzz And Automorphic no Program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 10 == 7 || number % 7 == 0)
+            {
+                Console.WriteLine(" number is Buzz Number");
+            }
+            else
+            {
+                Console.WriteLine("number is Not a Buzz Number");
+            }
+            Class1 obj = new Class1();
+            obj.CheckAutoMorpicNumber();
+
+        }
+
+
+    }
+    }
