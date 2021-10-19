e315144429f59f2615f06d73e0c36afbb21f8161
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-10-19 22:54:17 +0530

    [Rupali]Feat:added bubble sort program for string

--- Program.cs
+++ Program.cs
@@ -11,19 +11,19 @@ namespace ConsoleApp12
 
             Console.WriteLine("Enter size of Array : ");
             int size = int.Parse(Console.ReadLine());
-            int[] a = new int [size];
+            string [] a = new string [size];
             Console.WriteLine("Array Elements Are : ");
             for (int i = 0; i <a.Length; i++)
             {
-                a[i] = int.Parse(Console.ReadLine());
+                a[i] = (Console.ReadLine());
             }
             Console.WriteLine("After Sorting elements are : ");
-            int temp;
+            string temp;
             for (int i = 0; i < a.Length; i++)
             {
                 for (int j = 0; j < a.Length-1-i; j++)
                 {
-                    if (a[j] > a[j+1])
+                    if (a[j].CompareTo(a[j + 1]) > 0) 
                     {
                         temp = a[j];
                         a[j] = a[j + 1];
