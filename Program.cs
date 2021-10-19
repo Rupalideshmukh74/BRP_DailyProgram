64ce9e95d270f0e3802efd87baa4a3edff640325
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-10-19 23:26:06 +0530

    [Rupali]Feat:insertion of element in an Array

--- Program.cs
+++ Program.cs
@@ -9,36 +9,29 @@ namespace ConsoleApp12
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter size of Array : ");
+            Console.WriteLine("Enter size of an Array : ");
             int size = int.Parse(Console.ReadLine());
-            string [] a = new string [size];
+
+            int[] a = new int[size];
+
             Console.WriteLine("Array Elements Are : ");
-            for (int i = 0; i <a.Length; i++)
-            {
-                a[i] = (Console.ReadLine());
-            }
-            Console.WriteLine("After Sorting elements are : ");
-            string temp;
             for (int i = 0; i < a.Length; i++)
             {
-                for (int j = 0; j < a.Length-1-i; j++)
-                {
-                    if (a[j].CompareTo(a[j + 1]) > 0) 
-                    {
-                        temp = a[j];
-                        a[j] = a[j + 1];
-                        a[j + 1] = temp;
-                    }
-
-                }
-
+                a[i] = int.Parse(Console.ReadLine());
             }
+
+            Console.WriteLine("After Adding new Element : ");
+            int pos = 3;
+            int element = 100;
+            for (int i = a.Length-1; i >pos-1; i--)
+            {
+                a[i] = a[i - 1];
+            }
+            a[pos - 1] = element;
             for (int i = 0; i < a.Length; i++)
             {
-                Console.WriteLine(a[i] + " ");
+                Console.WriteLine(a[i]+" ");
             }
-            
-
 
         }
 
